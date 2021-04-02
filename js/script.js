152baf582052c76bbd75edf3a87fc9e73db7fd58
Josh Taylor <josh@joshtaylordesign.com>
2021-04-01 17:23:04 -0700

    working js storing function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,77 @@
 // DAY PLANNER HOMEWORK
-// Setup HTML
-  //get moment and bootstrap cdn links (for bootstrap:  css and JS cdn link from the docs)
-  //layout the html
-    //add a jumbotron that display current day and date using moment (e.g. Wed, Mar 31st)
-  //create rows for hours (9am-5pm)
-    //parent div / hour div / text input /save button
-//Start JavaScript
 
 
 
+//set time refresh at top of page
 setInterval(function(){
 var time = moment().format("hh:mm:ss");
 $("#settime").text(time);
 })
 
 
+//shortcuts to parts of schedule
+
+
+//Set global variables
+var dayArray = ['','','','','','','','','',''];
+var tempDayArray = [];
+
+
+
+
+
+
+function getHour() {
+
+}
+
+function saveHour() {
+
+}
+
+function initDay() {
+  //get dayArray from local storage
+  tempDayArray = JSON.parse(localStorage.getItem('dayArray'));
+  dayArray = tempDayArray;
+
+  //loop statement to fill each hour from dayArray
+  for (i=0; i<10; i++) {
+    $('#'+i).children('input').attr('placeholder', dayArray[i]);
+    }
+
+}
+
+initDay();
+
+
+
   //setup a click event on the save buttons
-$('.save').on("click", function(event){
-    $(this).style.backgroundcolor('#111111');
+$('button').on("click", function(event){
+  var hourInput;
+  //var textfield = this.siblings();
+  $(this).siblings().eq(1).attr('placeholder', 'testing!');
+  hourInput = $(this).siblings().eq(1).val();
+  //targets the correct spot in the dayArray to add the text
+  dayArray[$(this).attr('id')] = hourInput;
+  localStorage.setItem('dayArray', JSON.stringify(dayArray));
 })
 
+// Starting from `<div id="top">`, get a list of its sibling elements and target the fourth one (the `<ul>` element), and append an `<li>` to add "Classmates" to bottom of the list as a new resource.
+//$('#top').siblings().eq(4).append($('<li>Classmates</li>'));
 
 
+//     localStorage.setItem("dayArray", enteredTextField.val());
+//     $(this).parent().css('background-color', 'rgb(122, 242, 242)');
+// })
 
+
+// var enteredTextField = $('#input8');
+// enteredTextField.css('border-color','rgb(122, 242, 242)');
+// var enteredTextValue = enteredTextField.val();
+// console.log(enteredTextValue);
+
+// topScoresArray.push(currentScoreObject);
+// localStorage.setItem("initScore", JSON.stringify(topScoresArray));
 
 
 
